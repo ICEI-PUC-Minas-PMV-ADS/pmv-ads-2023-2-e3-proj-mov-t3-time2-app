55b0c7d0256a177bce0b2b3d6bf7d7528b8b398f
Junia Campos <113808083+JuniaMCampos@users.noreply.github.com>
2023-09-23 14:15:30 -0300

    Update banco.sql
    TABELAS BANCO DE DADOS

--- src/banco.sql
+++ src/banco.sql
@@ -1,1 +1,50 @@
+-- Tabela Usuário
 
+CREATE TABLE USER (
+  idUser INT PRIMARY KEY,
+  nome VARCHAR(255),
+  email VARCHAR(255),
+  senha VARCHAR(255),
+  idade INT(100),
+  cargo VARCHAR(255)
+  );
+
+-- Tabela Autenticação
+
+CREATE TABLE AUTH (
+  email VARCHAR(255),
+  senha VARCHAR(255)
+  );
+
+-- Tabela Projeto
+
+CREATE TABLE PROJETO (
+  idProjeto INT PRIMARY KEY,
+  nome VARCHAR(255),
+  descricao VARCHAR(255),
+  dataInicio DATE,
+  dataConclusao DATE,
+  task VARCHAR(255),
+  FOREIGN KEY (idUser) REFERENCES USER(idUser)
+  );
+
+-- Tabela Tarefas
+
+CREATE TABLE TASK (
+  idTask INT PRIMARY KEY,
+  descricao VARCHAR(255),
+  status VARCHAR(255),
+  dataInicio DATE,
+  dataConclusao DATE,
+  responsavel VARCHAR(255),
+  FOREIGN KEY (idUser) REFERENCES USER(idUser)
+  );
+
+-- Tabela Documentos
+
+CREATE TABLE DOCUMENTO (
+  extensaoDoc VARCHAR(255),
+  nomeDoc VARCHAR(255),
+  FOREIGN KEY (idUser) REFERENCES USER(idUser)
+  );
+  
